4bbad5d97b8e76d20489110aa439215ef870a4d9
pipeline <pipeline@syncfusion.com>
2018-02-22 03:22:29 +0000

    v16.1.24-preview is released

---
+++ dist/global/index.d.ts
@@ -0,0 +1,41 @@
+/*!
+*  filename: index.d.ts
+*  version : 15.4.30
+*  Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
+*  Use of this code is subject to the terms of our license.
+*  A copy of the current license can be obtained at any time by e-mailing
+*  licensing@syncfusion.com. Any infringement will be prosecuted under
+*  applicable laws. 
+*/
+
+import * as _grids from '@syncfusion/ej2-grids';
+import * as _base from '@syncfusion/ej2-base';
+import * as _data from '@syncfusion/ej2-data';
+import * as _navigations from '@syncfusion/ej2-navigations';
+import * as _inputs from '@syncfusion/ej2-inputs';
+import * as _calendars from '@syncfusion/ej2-calendars';
+import * as _dropdowns from '@syncfusion/ej2-dropdowns';
+import * as _popups from '@syncfusion/ej2-popups';
+import * as _buttons from '@syncfusion/ej2-buttons';
+import * as _lists from '@syncfusion/ej2-lists';
+import * as _excelexport from '@syncfusion/ej2-excel-export';
+import * as _pdfexport from '@syncfusion/ej2-pdf-export';
+import * as _fileutils from '@syncfusion/ej2-file-utils';
+import * as _compression from '@syncfusion/ej2-compression';
+
+export declare namespace ej {
+    const grids: typeof _grids;
+    const base: typeof _base;
+    const data: typeof _data;
+    const navigations: typeof _navigations;
+    const inputs: typeof _inputs;
+    const calendars: typeof _calendars;
+    const dropdowns: typeof _dropdowns;
+    const popups: typeof _popups;
+    const buttons: typeof _buttons;
+    const lists: typeof _lists;
+    const excelexport: typeof _excelexport;
+    const pdfexport: typeof _pdfexport;
+    const fileutils: typeof _fileutils;
+    const compression: typeof _compression;
+}
